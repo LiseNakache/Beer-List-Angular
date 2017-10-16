7369967394b5f450f10bd966c1c2704dd6a4ae4d
Lise <nakache.lise@gmail.com>
2017-10-16 11:05:50 +0300

    second commit

--- beerModel.js
+++ beerModel.js
@@ -1,1 +1,13 @@
+var mongoose = require('mongoose');
 
+var beerSchema = new mongoose.Schema({
+    name: { type: String },
+    style: { type: String },
+    image_url: { type: String },
+    abv: { type: Number },
+    ratings: [Number]
+});
+
+var Beer = mongoose.model('beer', beerSchema)
+
+module.exports = Beer;
--- server.js
+++ server.js
@@ -1,12 +1,68 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/beers')
+
 var app = express();
+
+var Beer = require('./beerModel');
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/beers', function (req, res, next) {
+  Beer.create(req.body, function(err, beer) {
+    if (err) {
+      return next(err);
+    } else {
+      return res.send(beer);
+      res.send('POST!');
+    }
+  });
+});
+
+app.get('/beers',function(req, res, next){
+  Beer.find({},function(err,newBeer){
+    if(err){ return next(error);}
+    console.log(newBeer)
+    res.send(newBeer)
+  })
+})
+
+app.delete('/beers/:id',function(req, res, next){
+  Beer.findByIdAndRemove(req.params.id,function(err,newBeer){
+    if(err){ return next(error);}
+    console.log(newBeer)
+    res.send(newBeer)
+  })
+})
+
+
+
+
+// error handler to catch 404 and forward to main error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// main error handler
+// warning - not for use in production code!
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: err
+  });
+});
+
+
+
 
 app.listen(8000, function() {
   console.log("yo yo yo, on 8000!!")
